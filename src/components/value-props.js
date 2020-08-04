57e70ba1b7c3dc7dd6d36f44ed1e28325875bace
sam <s.ojling@gmail.com>
2020-08-04 23:12:52 +0200

    Fixed value props on mobile

--- src/components/value-props.js
+++ src/components/value-props.js
@@ -6,6 +6,11 @@ import smileIcon from 'images/smile-icon.svg'
 const valuesOneDesktop = ['0 Sugar', '0 Calories', '0 Sodium']
 const valuesTwoDesktop = ['Gluten free', 'Non GMO', 'Vegan', 'Plant based']
 const valuesThreeDesktop = ['No artificial flavors', '1% for the Planet']
+// Mobile
+const valuesOneMobile = ['0 Sugar', '0 Calories']
+const valuesTwoMobile = ['0 Sodium', 'Gluten free', 'Non GMO']
+const valuesThreeMobile = ['Vegan', 'Plant based']
+const valuesFourMobile = ['No artificial flavors', '1% for the Planet']
 
 const ValueProp = ({ valueProp }) => (
   <div className="d-flex">
@@ -14,7 +19,7 @@ const ValueProp = ({ valueProp }) => (
       src={smileIcon}
       alt="Happy smiling face"
     />
-    <p className="font-barlow margin-bottom-none body-3">{valueProp}</p>
+    <p className="font-barlow margin-bottom-none body-4">{valueProp}</p>
   </div>
 )
 
@@ -22,7 +27,7 @@ const ValueProps = () => {
   return (
     <section className="bg-lightYellow padding-top-30px padding-bottom-20px padding-top-md-none padding-bottom-md-40px">
       <div className="container padding-top-none padding-bottom-10px padding-top-md-none padding-bottom-md-30px">
-        <div className="row">
+        <div className="row d-none d-sm-flex">
           <div className="col-12 align-items-center padding-bottom-15px justify-content-center d-flex value-props-row">
             {valuesOneDesktop.map((valueProp, index) => (
               <ValueProp valueProp={valueProp} key={index} />
@@ -35,6 +40,28 @@ const ValueProps = () => {
           </div>
           <div className="col-12 align-items-center  justify-content-center d-flex value-props-row">
             {valuesThreeDesktop.map((valueProp, index) => (
+              <ValueProp valueProp={valueProp} key={index} />
+            ))}
+          </div>
+        </div>
+        <div className="row d-sm-none">
+          <div className="col-12 align-items-center padding-bottom-5px padding-bottom-lg-15px justify-content-center d-flex value-props-row">
+            {valuesOneMobile.map((valueProp, index) => (
+              <ValueProp valueProp={valueProp} key={index} />
+            ))}
+          </div>
+          <div className="col-12 align-items-center padding-bottom-5px padding-bottom-lg-15px justify-content-center d-flex value-props-row">
+            {valuesTwoMobile.map((valueProp, index) => (
+              <ValueProp valueProp={valueProp} key={index} />
+            ))}
+          </div>
+          <div className="col-12 align-items-center padding-bottom-5px padding-bottom-lg-15px justify-content-center d-flex value-props-row">
+            {valuesThreeMobile.map((valueProp, index) => (
+              <ValueProp valueProp={valueProp} key={index} />
+            ))}
+          </div>
+          <div className="col-12 align-items-center padding-bottom-5px padding-bottom-lg-15px justify-content-center d-flex value-props-row">
+            {valuesFourMobile.map((valueProp, index) => (
               <ValueProp valueProp={valueProp} key={index} />
             ))}
           </div>
