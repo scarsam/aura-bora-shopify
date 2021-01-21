055b5ca95895d3bdd207ed4a85fdbaf03cbaadac
sam <s.ojling@gmail.com>
2021-01-21 10:57:48 +0100

    Added loading on the index page

---
+++ src/pages/index-copy.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react'
+import Layout from 'components/layout'
+import { ProductGrid } from 'components/ProductGrid'
+import Hero from 'components/hero'
+import SEO from 'components/seo'
+import ValueProps from 'components/value-props'
+import Banner from 'components/ShopBanner'
+import PartnerBanner from "components/PartnerBanner"
+
+const IndexPage = () => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleFirstTab)
+    document.body.setAttribute('ontouchstart', '')
+    return () => {
+      window.removeEventListener('keydown', handleFirstTab)
+    }
+  })
+
+  function handleFirstTab(e) {
+    if (e.keyCode === 9) {
+      document.body.classList.add('user-is-tabbing')
+      window.removeEventListener('keydown', handleFirstTab)
+    }
+  }
+  return (
+    <>
+      <Banner />
+      <Layout>
+        <i className="font-space-mono absolute italic-font-loader">
+          <b>Loading italic font..</b>
+        </i>
+        <SEO title="Home" />
+        <Hero>
+          <div className="text-center padding-top-10px padding-top-md-none margin-bottom-5px margin-bottom-md-55px">
+            <p className="margin-none body-2">
+              Aura Bora is a sparkling water{' '}
+              <span className="d-lg-block">
+                made from herbs, fruits, and flowers for{' '}
+              </span>
+              earthly tastes and heavenly feelings.
+            </p>
+          </div>
+        </Hero>
+        <ValueProps />
+        <ProductGrid />
+        <PartnerBanner />
+      </Layout>
+    </>
+  )
+}
+
+export default IndexPage
